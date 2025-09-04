dade3fc53b4827e353f0c8c6adecd2ee4847894b
Jordan Chin <jc9627@g.rit.edu>
2025-09-04 15:55:22 -0400

    added hero

---
+++ genre-web/src/components/Hero.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { HERO_IMAGE } from '../lib/config'
+
+const Hero = ({children} : {children: React.ReactNode}) => {
+  return (
+    <div className="min-h-screen w-full relative">
+        <div
+            className='absolute inset-0 bg-center bg-cover'
+            style={{backgroundImage: `url(${HERO_IMAGE})`}}
+            aria-hidden
+        />
+        <div className='absolute inset-0 bg-black/50'/>
+        <main className='relative z-10 min-h-screen flex items-center justify-center p-6'>
+            {children}
+        </main>
+    </div>
+  )
+}
+
+export default Hero
---
+++ genre-web/src/lib/config.ts
@@ -0,0 +1,6 @@
+export const HERO_IMAGE = "/assets/home.jpg";
+
+export const GENRE_IMAGES: Record<string, string> = {
+    blues: "assets/blues-image.jpg",
+    rock: "assets/rock-image.jpg",
+};
